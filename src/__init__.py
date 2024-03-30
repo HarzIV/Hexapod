f9eccad10f3a6488d72833cb5f6aadb220815279
Harz IV <HarzDerIV@gmail.com>
2024-03-30 17:58:51 +0100

    Removed Files

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,5 @@ from . import test_OOP
 from . import ConI2
 from . import ControllerInputs
 from . import GUI
-from . import inverse_Kinematics
-from . import model
-from . import Pattern
+from . import LegMath
 from . import Config
